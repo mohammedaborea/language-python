c394be619abf67b57d4ca6b601a73f56e634432a
mohammedaborea <mohammedaborea@gmail.com>
2026-06-07 18:56:38 +0100

    Create Ex1.py
    A simple Python program that asks the user for their name and displays a personalized welcome message.

---
+++ Basic /Ex1.py
@@ -0,0 +1,4 @@
+# Ce programme demande à l'utilisateur de saisir son nom,
+# puis affiche un message de bienvenue personnalisé.
+nom_de_utilisateur=input("Entrez votre nom : ")
+print ("Bienvenue Mr ", nom_de_utilisateur)

7229fe63c5b2eef3649e4dee8fc988432b370af6
mohammedaborea <mohammedaborea@gmail.com>
2026-06-07 19:44:25 +0100

    Create Ex6.py
    A simple program for determining whether a number is even or odd.

---
+++ Basic /Ex6.py
@@ -0,0 +1,6 @@
+print("  Programme de verification de la paritee ")
+nombre=int(input("Entrez un nombre entier pour verification : "))
+if nombre%2==0:
+    print(nombre,"est un nombre paire ")
+else:
+    print(nombre,"est un nombre impaire")
